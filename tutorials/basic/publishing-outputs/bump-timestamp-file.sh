147e0ac1bdb91e67fa25e5c748dc3494d50270fa
Samriddhi, Kaur <samriddhhik562@gmail.com>
2024-10-10 11:16:39 -0400

    adding another config

--- tutorials/basic/publishing-outputs/bump-timestamp-file.sh
+++ tutorials/basic/publishing-outputs/bump-timestamp-file.sh
@@ -12,5 +12,6 @@ date > bumpme
 git config --global user.email "nobody@concourse-ci.org"
 git config --global user.name "Concourse"
 
+git config --global --add safe.directory /tmp/build/cc4f7a9f/updated-gist
 git add .
 git commit -m "Bumped date"
