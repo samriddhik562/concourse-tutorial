3cedef3bb5baf9316e7016229e494ee8cb3c970b
Samriddhi, Kaur <samriddhhik562@gmail.com>
2024-10-10 11:13:26 -0400

    changes again.....

--- tutorials/basic/publishing-outputs/bump-timestamp-file.sh
+++ tutorials/basic/publishing-outputs/bump-timestamp-file.sh
@@ -3,6 +3,7 @@
 set -e # fail fast
 set -x # print commands
 
+git config --global --add safe.directory /tmp/build/cc4f7a9f/resource-gist/.git
 git clone resource-gist updated-gist
 
 cd updated-gist
